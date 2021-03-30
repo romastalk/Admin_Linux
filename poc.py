ec397a100ecc5592e5e04ca2f2ad0739ca71d80f
user <chirko-99@mail.ru>
2021-03-30 14:17:43 +0500

    сооб

--- poc.py
+++ poc.py
@@ -6,9 +6,11 @@ import paramiko
 def ssh_ls_example():
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname='192.168.210.9', port=22, username='user', password='user2020')
+    client.connect(hostname='192.168.210.11', port=22, username='user', password='user2020')
     stdin_raw, stdout_raw, stderr_raw = client.exec_command('ls')
     exit_code = stdout_raw.channel.recv_exit_status()
+
+# выключение пк sudo shutdown now + парва админа, включение ls
 
     stdout = []
     for line in stdout_raw:
@@ -23,7 +25,7 @@ def ssh_ls_example():
     client.close()
     del client, stdin_raw, stdout_raw, stderr_raw
 
-example_mac = 'b4:2e:99:8e:45:af'
+example_mac = 'b4:2e:99:85:1e:45'
 
 def wake_on_lan(mac):
     import os
@@ -31,5 +33,18 @@ def wake_on_lan(mac):
     output = stream.read()
     print(output)
 
+hostname = "192.168.210.11"
+
+def ping(ip):
+    import os
+    hostname = "192.168.210.11"
+    response = os.system("ping -c 4 " + hostname)
+    if response == 0:
+        print("Success")
+    else:
+        print("Not Success")
+
 
 wake_on_lan(example_mac)
+# ctr+/ комментарий
+ping (hostname)
