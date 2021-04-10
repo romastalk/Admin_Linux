bb21742514656ea6b80c0f08ff8045e9e6d5540e
user <chirko-99@mail.ru>
2021-04-10 14:12:24 +0500

    доработка

--- poc.py
+++ poc.py
@@ -10,6 +10,7 @@ def connect(ip, username, password):
     return client
 
 def exec(client, command):
+    print(f'>>>>> Выполняем [{command.strip()}] <<<<<')
     stdin_raw, stdout_raw, stderr_raw = client.exec_command(command)
     exit_code = stdout_raw.channel.recv_exit_status()
     stdout = []
@@ -21,41 +22,15 @@ def exec(client, command):
     print('\n'.join(stdout))
     del stdin_raw, stdout_raw, stderr_raw
 
-# def read():
-#     pc_dict = {}
-#     with open('info', 'r') as file:
-#         lines = file.readlines()
-#
-#     for line in lines:
-#         if line:
-#             pc_details = line.split()
-#             pc_dict[pc_details[0]] = {'ip': pc_details[1], 'mac': pc_details[2]}
-#     print(pc_dict)
-#     return pc_dict
-
-# pc_dict = read()
-# pc_list = []
-
 def read_script(ip, username, password):
     with open('script', 'r') as file:
         lines = file.readlines()
 
-    client=connect(ip=ip, username=username, password=password)
+    client = connect(ip=ip, username=username, password=password)
 
     for line in lines:
         exec(client, line)
     client_close(client)
-# выключение пк sudo shutdown now + парва админа, включение ls
-
-    # stdout = []
-    # for line in stdout_raw:
-    #     stdout.append(line.strip())
-    #
-    # stderr = []
-    # for line in stderr_raw:
-    #     stderr.append(line.strip())
-    #
-    # print(stdout)
 
 def client_close(client):
     client.close()
@@ -82,7 +57,8 @@ def ping(ip):
 
 
 read_script(ip="192.168.210.12", username="user", password="user2020")
-# read()
+
+# client = connect(ip="192.168.210.12", username="user", password="user2020")
 # exec(client, "ls")
 # client_close(client)
 
